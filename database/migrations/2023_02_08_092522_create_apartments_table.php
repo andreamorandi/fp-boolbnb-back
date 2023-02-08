bba1d7faff8a01a106c780363e42d348e9c17071
andreamorandi <morandiandrearocco@gmail.com>
2023-02-08 11:34:52 +0100

    first commit

---
+++ database/migrations/2023_02_08_092522_create_apartments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->tinyInteger('room_number');
+            $table->tinyInteger('bed_number');
+            $table->tinyInteger('bathroom_number');
+            $table->smallInteger('surface_sqm');
+            $table->string('full_adress', 255);
+            $table->string('image', 255)->nullable();
+            $table->tinyInteger('is_visible')->unsigned()->default(1);
+            $table->string('latitude', 50)->nullable();
+            $table->string('longitude', 50)->nullable();
+            $table->string('slug');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
